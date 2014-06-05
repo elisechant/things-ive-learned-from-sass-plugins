2dcacde9e3555ab21ba2660e55315b019329ac50
elisechant <elise.chant@gmail.com>
2014-06-05 15:30:03 +1000

    finished the fucking thing again

--- dist/assets/js/SP.main.js
+++ dist/assets/js/SP.main.js
@@ -1,4 +1,4 @@
-/*! things-ive-learned-from-sass-plugins Thursday, June 5th, 2014, 12:08:34 PM* Copyright (c) 2014 elisechant; */
+/*! things-ive-learned-from-sass-plugins Thursday, June 5th, 2014, 3:17:30 PM* Copyright (c) 2014 elisechant; */
 
 var SP = SP || (window.SP = {});
 
